03d52cd82878030b51405f806d9688d088e1246f
Jelle Raaijmakers <jelle@gmta.nl>
2013-02-10 23:21:38 +0100

    Implemented conflict redirection for paths equal to or residing within the metadata directory.

--- mountload/target.py
+++ mountload/target.py
@@ -6,54 +6,63 @@ from os.path import abspath, isdir
 
 class MountLoadTarget:
     def __init__(self, targetDirectory):
+        self.databaseFilename = 'metadata.sqlite'
         self.targetDirectory = abspath(targetDirectory)
         self.metaDirectory = self.targetDirectory + '/.mountload'
+        self.redirectionDirectory = self.metaDirectory + '/redirect'
 
-        # Create dirs if they don't already exist
-        if not isdir(self.targetDirectory):
-            os.mkdir(self.targetDirectory)
-        if not isdir(self.metaDirectory):
-            os.mkdir(self.metaDirectory)
+        self._ensureDirectoriesExist([self.targetDirectory, self.metaDirectory, self.redirectionDirectory])
 
     def close(self):
         pass
 
-    def createDirectory(self, path, mode):
-        dirpath = self._normalizePath(path)
+    def createDirectory(self, relativePath, mode):
+        dirpath = self._normalizePath(relativePath)
         if isdir(dirpath):
             os.chmod(dirpath, mode)
         else:
             os.mkdir(dirpath, mode)
 
-    def createFile(self, path, mode):
-        path = self._normalizePath(path)
+    def createFile(self, relativePath, mode):
+        path = self._normalizePath(relativePath)
         f = open(path, 'w')
         f.close()
         os.chmod(path, mode)
 
-    def createSymlink(self, path, target):
-        os.symlink(target, self._normalizePath(path))
+    def createSymlink(self, relativePath, target):
+        os.symlink(target, self._normalizePath(relativePath))
+
+    def _ensureDirectoriesExist(self, directories):
+        for directory in directories:
+            if not isdir(directory):
+                os.mkdir(directory)
 
     def getDBPath(self):
-        return self.metaDirectory + '/metadata.sqlite'
+        return self.metaDirectory + '/' + self.databaseFilename
 
-    def getSymlink(self, path):
-        return os.readlink(self._normalizePath(path))
+    def getSymlink(self, relativePath):
+        return os.readlink(self._normalizePath(relativePath))
 
-    def _normalizePath(self, path):
-        path = self.targetDirectory + path
-        # TODO: redirect for self.metaDirectory
-        return path
+    def _normalizePath(self, relativePath):
+        """
+        Constructs the absolute path for a path relative to the root of our
+        mountpoint. If this path would happen to conflict with our metadata
+        directory, we redirect the entire path to our redirections directory.
+        """
+        absolutePath = self.targetDirectory + relativePath
+        if absolutePath == self.metaDirectory or absolutePath.startswith(self.metaDirectory + '/'):
+            absolutePath = self.redirectionDirectory + relativePath
+        return absolutePath
 
-    def readData(self, path, offset, size):
-        f = open(self._normalizePath(path), 'rb')
+    def readData(self, relativePath, offset, size):
+        f = open(self._normalizePath(relativePath), 'rb')
         f.seek(offset, os.SEEK_SET)
         data = f.read(size)
         f.close()
         return data
 
-    def writeData(self, path, offset, data):
-        f = open(self._normalizePath(path), 'r+b')
+    def writeData(self, relativePath, offset, data):
+        f = open(self._normalizePath(relativePath), 'r+b')
         f.seek(offset, os.SEEK_SET)
         f.write(data)
         f.close()
