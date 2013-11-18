fa46f643f58d05e411c689f940407608e1d845ff
Jelle Raaijmakers <jelle@gmta.nl>
2013-11-19 00:19:44 +0100

    Renamed FUSEConnector.'controllerPool' to 'pool'.

--- mountload/fuseconnector.py
+++ mountload/fuseconnector.py
@@ -7,7 +7,7 @@ import logging
 
 class FUSEConnector(LoggingMixIn, Operations):
     def __init__(self, controllerPool, isDebugMode):
-        self.controllerPool = controllerPool
+        self.pool = controllerPool
 
         # Setup logger
         loglevel = logging.DEBUG if isDebugMode else logging.WARNING
@@ -18,28 +18,28 @@ class FUSEConnector(LoggingMixIn, Operations):
         self.log.addHandler(sh)
 
     def destroy(self, path):
-        self.controllerPool.close()
+        self.pool.close()
 
     def getattr(self, path, fh=None):
-        with self.controllerPool.acquire() as controller:
+        with self.pool.acquire() as controller:
             attr = controller.getStatForPath(path)
         if attr is None:
             raise FuseOSError(ENOENT)
         return attr
 
     def read(self, path, size, offset, fh):
-        with self.controllerPool.acquire() as controller:
+        with self.pool.acquire() as controller:
             return controller.readData(path, offset, size)
 
     def readdir(self, path, fh):
         entries = ['.', '..']
-        with self.controllerPool.acquire() as controller:
+        with self.pool.acquire() as controller:
             for entry in controller.getEntriesInDirectory(path):
                 entries.append(entry['basename'])
         return entries
 
     def readlink(self, path):
-        with self.controllerPool.acquire() as controller:
+        with self.pool.acquire() as controller:
             return controller.getSymlinkTarget(path)
 
     def startFUSE(self, mountpoint, isDaemonized=True, isMultiThreaded=False):
