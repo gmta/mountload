da5232de25c06f28284c0096275ee555ec4f7cc5
Jelle Raaijmakers <jelle@gmta.nl>
2013-11-21 01:06:36 +0100

    Split the argument parser into two groups: mountload arguments and FUSE arguments.

--- mountload/mountload.py
+++ mountload/mountload.py
@@ -12,13 +12,18 @@ class MountLoad:
         """Handles command line parameters, configuration and sets up the mountload components"""
 
         # Parse the command line arguments
-        parser = ArgumentParser(description='Mountload mounts a remote directory using SFTP while also downloading it to another target directory.')
-        parser.add_argument('--debug', action='store_true', help="Enable debug mode")
-        parser.add_argument('--multithreaded', action='store_true', help="Run FUSE in multithreaded mode")
-        parser.add_argument('--password', action='store_true', help="Ask for an SSH password")
-        parser.add_argument('source', help="The SFTP source URI, eg: sftp://user@example.org/path/to/remote/dir", nargs='?')
-        parser.add_argument('target', help="The directory in which all the files should be stored")
-        parser.add_argument('mountpoint', help="Path to the mountpoint")
+        parser = ArgumentParser(description='Mountload mounts a remote directory using SFTP and simultaneously downloads it to another target directory.')
+
+        grp_ml = parser.add_argument_group('Mountload arguments')
+        grp_ml.add_argument('--debug', action='store_true', help="Enable debug mode")
+        grp_ml.add_argument('--password', action='store_true', help="Ask for an SSH password")
+        grp_ml.add_argument('source', help="The SFTP source URI, eg: sftp://user@example.org/path/to/remote/dir", nargs='?')
+        grp_ml.add_argument('target', help="The directory in which all the files should be stored")
+        grp_ml.add_argument('mountpoint', help="Path to the mountpoint")
+
+        grp_fuse = parser.add_argument_group('FUSE arguments')
+        grp_fuse.add_argument('--multithreaded', action='store_true', help="Use multiple threads for filesystem access")
+
         args = parser.parse_args()
 
         # Determine configuration
