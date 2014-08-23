b987e171baac064ecf8ff7175145fe07744a46ac
Jelle Raaijmakers <jelle@gmta.nl>
2014-08-23 19:59:23 +0200

    Add Paramiko as a submodule in the new /vendor/ subdirectory.
    Also, move fuse.py there.

--- mountload.py
+++ mountload.py
@@ -2,6 +2,11 @@
 # Copyright (c) 2013 Jelle Raaijmakers <jelle@gmta.nl>
 # See the file license.txt for copying permission.
 
+# Add paths to dependencies
+import sys
+sys.path.append('vendor/fusepy/')
+sys.path.append('vendor/paramiko/')
+
 from mountload.mountload import MountLoad
 
 # Run mountload
